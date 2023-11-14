0a17e45d545a8d416e2723e54a03e8c885b33a84
“Edwin <oriokie@icloud.com>
2023-11-14 21:01:05 +0300

    new file:   monty.h

---
+++ monty.h
@@ -0,0 +1,69 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/*Critical Headers*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+/*Provided Data structures*/
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+		char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**created typedef*/
+
+typedef struct global_s{
+	stack_t *stack;
+	int line_number;
+	char *opcode;
+	FILE *file;
+	char *filename;
+	char *line;
+	char **args;
+} global_t;
+
+
+/*Global variables*/
+extern global_t global;
+
+
+/* Prototypes */
+
+
+
+
+#endif/* Monty header */
